da08e67e745427abcb888707af97234647cea625
Grant McGrath <grantmcgrath@me.com>
2017-07-18 15:33:06 -0400

    Completed assignment

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ function vCard() {
   let data = JSON.parse(this.responseText);
   // Creates the html in the index.html
   let html = `
-  <header>
+  <header class = "border">
     <h1>${data.name}</h1>
   </header>
   <main>
-    <section>
-      <div id = "basics">
+    <section class = "border">
+      <div id = "basics" class = "border">
         <h2>The Basics</h2>
         <ul>
           <li><label>Name: </label>${data.name}</li>
@@ -26,17 +26,15 @@ function vCard() {
           <li><label>Website: </label><a href=${data.blog}>${data.blog}</a></li>
         </ul>
       </div>
-      <div id = "story">
+      <div id = "story" class = "border">
         <h2>The Story</h2>
         <p>${data.bio}</p>
       </div>
-      <div id  = "avatar">
+      <div id  = "avatar" class = "border">
         <img src=${data.avatar_url} alt = "${data.name}'s profile image.'"
       </div>
     </section>
   </main>
-
-
 `;
   // Inserts html into the body tag of index.html
   body.innerHTML = html;
