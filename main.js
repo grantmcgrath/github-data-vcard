f448a8affed8239afd626accd741f25cf1c408b6
Grant McGrath <grantmcgrath@me.com>
2017-07-18 12:03:51 -0400

    uploading partcially completed project. Forgot to commit at creation of files.

---
+++ main.js
@@ -0,0 +1,43 @@
+let body = document.querySelector("body");
+
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/grantmcgrath");
+req.addEventListener("load", vCard);
+req.send();
+
+function vCard() {
+
+  // Parses the response text
+  let data = JSON.parse(this.responseText);
+  // Creates the html in the index.html
+  let html = `
+  <header>
+    <h1>${data.name}</h1>
+  </header>
+  <main>
+    <section>
+      <div id = "basics">
+        <h2>The Basics</h2>
+        <ul>
+          <li><label>Name: </label>${data.name}</li>
+          <li><label>GitHub URL: </label><a href=${data.html_url}>${data.login}</a></li>
+          <li><label>Email: </label>${data.email}</li>
+          <li><label>Company: </label>${data.company}</li>
+          <li><label>Website: </label><a href=${data.blog}>${data.blog}</a></li>
+        </ul>
+      </div>
+      <div id = "story">
+        <h2>The Story</h2>
+        <p>${data.bio}</p>
+      </div>
+      <div id  = "avatar">
+        <img src=${data.avatar_url} alt = "${data.name}'s profile image.'"
+      </div>
+    </section>
+  </main>
+
+
+`;
+  // Inserts html into the body tag of index.html
+  body.innerHTML = html;
+}
